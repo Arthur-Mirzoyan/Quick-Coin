1d099b4a594bb611129b74c12790e1ac97ba83d5
Arthur-Mirzoyan <artur.mirzoyan2.y@tumo.org>
2024-03-12 23:32:05 +0400

    changed my balance to progress, updated progress screen

---
+++ src/app/screens/Progress/Progress.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { styles } from './Progress.style';
+// Custom
+import { SubjectProgressCard } from '@components/SubjectProgressCard/SubjectProgressCard';
+
+function ProgressScreen() {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <SubjectProgressCard title="English" coinsGained={0} totalCoins={100} />
+      <SubjectProgressCard title="History" coinsGained={32} totalCoins={100} />
+      <SubjectProgressCard title="Maths" coinsGained={100} totalCoins={100} />
+      <SubjectProgressCard title="Armenian" coinsGained={90} totalCoins={100} />
+    </ScrollView>
+  );
+}
+
+export default ProgressScreen;
